4096373476a42a79ae7d8a02f0a9ba042eb37eb5
Sean Harte <scsharte@gmail.com>
2021-07-02 07:32:38 -0400

    added image sizes

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = {
+  upload: {
+    breakpoints: {
+      xxlarge: 1920,
+      xlarge: 1440,
+      large: 1280,
+      medium: 1024,
+      small: 768,
+      xsmall: 414,
+      largeIcon: 512,
+      mediumIcon: 192,
+      smallIcon: 72
+    }
+  }
+}
+ 
