277740c1e4d01e0e702888f855550d8d19e5a752
Sean Harte <scsharte@gmail.com>
2021-07-12 20:51:02 -0400

    set up email provider sendingblue

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,13 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  email: {
+    provider: 'sendinblue',
+    providerOptions: {
+        sendinblue_api_key: env('SIB_API_KEY'),
+        sendinblue_default_replyto: env('SIB_DEFAULT_REPLY_TO'),
+        sendinblue_default_from: env('SIB_DEFAULT_FROM'),
+        sendinblue_default_from_name: env('SIB_DEFAULT_FROM_NAME'),
+    },
+  },
   upload: {
     breakpoints: {
       xxlarge: 1920,
@@ -12,5 +21,5 @@ module.exports = {
       smallIcon: 72
     }
   }
-}
+})
  
