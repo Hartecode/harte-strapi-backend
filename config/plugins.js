3d193a451e8e658f0715b2ed3817c828779e198f
Sean Harte <scsharte@gmail.com>
2021-07-18 14:08:22 -0400

    added responsive image plugin

--- config/plugins.js
+++ config/plugins.js
@@ -7,19 +7,6 @@ module.exports = ({ env }) => ({
         sendinblue_default_from: env('SIB_DEFAULT_FROM'),
         sendinblue_default_from_name: env('SIB_DEFAULT_FROM_NAME'),
     },
-  },
-  upload: {
-    breakpoints: {
-      xxlarge: 1920,
-      xlarge: 1440,
-      large: 1280,
-      medium: 1024,
-      small: 768,
-      xsmall: 414,
-      largeIcon: 512,
-      mediumIcon: 192,
-      smallIcon: 72
-    }
   }
 })
  
