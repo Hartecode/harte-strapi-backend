73cd3d6c775a2252fcb0e259748b2aeffae3c996
Sean Harte <scsharte@gmail.com>
2021-07-25 21:47:17 -0400

    Added cloundary as a provider

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,14 @@ module.exports = ({ env }) => ({
         sendinblue_default_from: env('SIB_DEFAULT_FROM'),
         sendinblue_default_from_name: env('SIB_DEFAULT_FROM_NAME'),
     },
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
   }
 })
  
