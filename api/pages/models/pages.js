3996038fe40b2c8c08d2ad5c66f6a424c5adf67e
Sean Harte <scsharte@gmail.com>
2021-08-03 13:45:57 -0400

    updated lifecycle logic to work on create and update

--- api/pages/models/pages.js
+++ api/pages/models/pages.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function generateFullPath(data) {
+  if (data.slug && !data.homePage) {
+    const path = []
+    if(data.category) {
+      const entity = await strapi.query("category").find({ _id: data.category });
+    }
+    path.push(data.slug)
+    data.path = `/${path.join('/')}`
+  } else if (data.homePage) {
+    data.path = '/'
+  } else {
+    data.path = ''
+  }
+}
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
@@ -7,20 +22,12 @@
 
 module.exports = {
   lifecycles: {
+    beforeCreate: async (data) => {
+      generateFullPath(data)
+    },
     // auto update the path field 
-    async beforeUpdate(params, data) {
-      if (data.slug && !data.homePage) {
-        const path = []
-        if(data.category) {
-          const entity = await strapi.services.category.find({ _id: data.category });
-        }
-        path.push(data.slug)
-        data.path = `/${path.join('/')}`
-      } else if (data.homePage) {
-        data.path = '/'
-      } else {
-        data.path = ''
-      }
+    beforeUpdate: async (params, data) => {
+      generateFullPath(data)
     },
   },
 };
