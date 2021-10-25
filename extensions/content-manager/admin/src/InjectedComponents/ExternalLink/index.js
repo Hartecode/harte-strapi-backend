75d3d9456958a576c2426e5e76f09d0cdf889b17
Sean Harte <>
2021-10-24 20:36:22 -0400

    preview button to pages

---
+++ extensions/content-manager/admin/src/InjectedComponents/ExternalLink/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useStrapi, formatComponentData, useContentManagerEditViewDataManager } from 'strapi-helper-plugin';
+import EyeIcon from './view.svg';
+
+const StyledExternalLink = styled.a`
+  display: block;
+  color: #333740;
+  width: 100%;
+  text-decoration: none;
+  span,
+  i,
+  svg {
+    color: #333740;
+    width: 13px;
+    height: 12px;
+    margin-right: 10px;
+    vertical-align: 0;
+  }
+  span {
+    font-size: 13px;
+  }
+  i {
+    display: inline-block;
+    background-image: url(${EyeIcon});
+    background-size: contain;
+  }
+  &:hover {
+    text-decoration: none;
+    span,
+    i,
+    svg {
+      color: #007eff;
+    }
+  }
+`;
+
+const ExternalLink = () => {
+    const CLIENT_PREVIEW_URL= 'http://localhost:3000';
+    const CLIENT_PREVIEW_SECRET = '****';
+
+    const { allLayoutData, modifiedData, layout }= useContentManagerEditViewDataManager();
+
+    // only have preview mode for pages
+    if (layout.apiID !== 'pages') {
+        return null
+    }
+
+    if (modifiedData.published_at) {
+        return null;
+    }
+    console.log( JSON.stringify(allLayoutData));
+
+    if (!modifiedData.slug) {
+        return null;
+    }
+
+    if (!CLIENT_PREVIEW_URL || !CLIENT_PREVIEW_SECRET) {
+        return null;
+    }
+
+    return (
+        <li>
+        <StyledExternalLink
+            href={`${CLIENT_PREVIEW_URL}/api/preview?secret=${CLIENT_PREVIEW_SECRET}&slug=${modifiedData.slug}&locale=${modifiedData.locale}&apiID=${layout.apiID}&kind=${layout.kind}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="page preview"
+        >
+            <i />
+            Preview
+        </StyledExternalLink>
+        </li>
+    );
+};
+
+export default ExternalLink;
